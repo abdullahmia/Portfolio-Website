ae2fa424616fc5d2b20c64dfdd58250c406c758c
darknoob1971 <abdullah149161@bsdi-bd.org>
2021-05-09 04:32:15 +0600

    fix Error

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Intro, Skill, SocialAccount, Project
 
 def home(request):
-    intro = Intro.objects.get(pk=1)
+    intro = Intro.objects.get(pk=2)
     skills = Skill.objects.all()
     socialaccounts = SocialAccount.objects.all()
     projects = Project.objects.all()
